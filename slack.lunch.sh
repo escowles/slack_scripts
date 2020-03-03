0ab270793cf939d934b30584cb24e8cda0b539ee
Esmé Cowles <escowles@ticklefish.org>
2020-03-03 07:01:40 -0500

    Randomizing lunch emoji

--- slack.lunch.sh
+++ slack.lunch.sh
@@ -3,8 +3,12 @@
 if [ "$1" ]; then
   EMOJI=$1
 else
-  EMOJI=pizza
+  FOOD_EMOJI="burrito hamburger pancakes pizza sandwich stew taco"
+  COUNT=`echo "$FOOD_EMOJI" | wc -w`
+  IDX=$(( ($RANDOM * $COUNT / 32768) + 1 ))
+  EMOJI=`echo "$FOOD_EMOJI" | cut -d\  -f$IDX`
 fi
 
 slack.out.sh
 slack.status.sh $EMOJI lunch
+echo $EMOJI
