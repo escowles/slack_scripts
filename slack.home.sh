b0ef21c55365158db614a6aea69643dc5f316755
Esmé Cowles <escowles@ticklefish.org>
2020-03-03 07:02:20 -0500

    Adding home and sick status shortcuts

---
+++ slack.home.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+slack.client.sh users.setPresence?presence=auto
+slack.status.sh house_with_garden "working from home"
---
+++ slack.sick.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+slack.client.sh users.setPresence?presence=away
+slack.status.sh face_with_thermometer "out sick"
