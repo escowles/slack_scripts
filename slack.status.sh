3f36b950d0721d0974c4357af501a706fe3e84ab
Esmé Cowles <escowles@ticklefish.org>
2020-09-01 07:21:19 -0400

    Updating slack.status.sh to output current status if no arguments are given

--- slack.status.sh
+++ slack.status.sh
@@ -3,8 +3,11 @@
 EMOJI=$1
 if [ "$EMOJI" ]; then
   EMOJI=":$EMOJI:"
+  shift
+  JSON_RAW="{\"status_emoji\":\"$EMOJI\",\"status_text\":\"$@\"}"
+  JSON_ENC=`echo "$JSON_RAW" | urlencode`
+  URL="users.profile.set?profile=$JSON_ENC"
+else
+  URL="users.profile.get?"
 fi
-shift
-JSON_RAW="{\"status_emoji\":\"$EMOJI\",\"status_text\":\"$@\"}"
-JSON_ENC=`echo "$JSON_RAW" | urlencode`
-slack.client.sh "users.profile.set?profile=$JSON_ENC" | jq ".profile | {"status": .status_text, "emoji": .status_emoji}"
+slack.client.sh "$URL" | jq ".profile | {"status": .status_text, "emoji": .status_emoji}"
